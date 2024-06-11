856af782a14740f53a8d5c95363e2097992c14cd
Tony Collins <tjc@eltham-college.org.uk>
2024-06-11 16:49:01 +0100

    Add project files.

---
+++ WpfApp4/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    class Board
+    {
+        private int colour = 5;
+        public Board()
+        {
+
+        }
+
+        public void ChangeSomething()
+        {
+
+            colour = colour + 1;
+        }
+        public int col
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+    }
+}
